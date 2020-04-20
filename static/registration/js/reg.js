f9a28fd2af152cf166698d18b2ec44457615dcef
Martin <bermudez_martin.nqn@hotmail.com>
2020-04-20 15:48:46 -0300

    css required_email hecho. Boton iniciar sesion facebook con estilo de fb.

--- static/registration/js/reg.js
+++ static/registration/js/reg.js
@@ -1,3 +1,23 @@
-function showForm() {
-	$('.form-create-user').css({'display':'block'});
+/*Para mostrar el modo teatro si se visita una publicacion invocando el get.*/
+$(document).ready(function() {
+    var params = getParameters();
+    for (var i = params.length - 1; i >= 0; i--) {
+        if (params[i][0]=='used' && params[i][1]=='True'){
+        	$('.msg').html('<p>El e-mail asociado a esta cuenta ya está en uso, por favor ingrese un nuevo e-mail.</p>')
+        }
+    }
+});
+
+/*funcion para obtener un arreglo con los parametros de la url*/
+function getParameters() {
+  var res=[];
+  if (location.search){
+    location.search.substr(1).split("&").forEach(function(param) {
+        var s = param.split("="), //separamos llave/valor
+        ll = s[0],
+        v =  decodeURIComponent(s[1]);
+        res.push([ll,v]); //si es nula, quiere decir que no tiene valor, solo textual
+      });
+  }
+  return res;
 }
