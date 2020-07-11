1fb5ad1e0a813796e770fd1b53edf4010ab5b9ee
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-07-11 10:28:18 -0300

    detalles minimos.

--- st-front/src/components/StoryTree.jsx
+++ st-front/src/components/StoryTree.jsx
@@ -9,12 +9,13 @@ const StoryTree = (props) => {
 
     async function fetchData() {
         var url = '';
-        if ('story'==props.type){
-            url = "/user/"+props.username+"/publication/continuations-titles/"+pub;
-        }else{
-            url = "/user/"+props.username+"/publication/continuations-chapter-titles/"+pub;
+        if ('story' == props.type) {
+            url = "/user/" + props.username + "/publication/continuations-titles/" + pub;
+        } else {
+            url = "/user/" + props.username + "/publication/continuations-chapter-titles/" + pub;
         }
-        const res = await fetch(url, {method: "GET",
+        const res = await fetch(url, {
+            method: "GET",
             headers: {
                 "X-Requested-With": "XMLHttpRequest",
                 "X-CSRFToken": getCookie("csrftoken"),
@@ -24,35 +25,35 @@ const StoryTree = (props) => {
             credentials: 'same-origin',
         });
         res
-        .json()
-        .then(res => {setData(res); console.log(res)})
-        .catch(err => setErrors(err));
+            .json()
+            .then(res => { setData(res); console.log(res) })
+            .catch(err => setErrors(err));
     }
 
     useEffect(() => {
         setData([]);
         setPub(props.pubId)
-    },[props.pubId]);
+    }, [props.pubId]);
 
     const callFirstChapters = () => {
         fetchData();
     }
 
     var childNodes = null;
-    if (data.childs != undefined && data.childs.length > 0){
+    if (data.childs != undefined && data.childs.length > 0) {
         childNodes = data.childs.map((child) =>
             <StoryTree username={props.username}
-            pubId={child.pubId}
-            title={child.title}
-            type='chapter' key={child.pubId} />
-            );
+                pubId={child.pubId}
+                title={child.title}
+                type='chapter' key={child.pubId} />
+        );
     }
 
     return <>
-    <li onClick={callFirstChapters}>
-    {props.title}
-    {{childNodes} && <ul>{childNodes}</ul>}
-    </li>
+        <li onClick={callFirstChapters}>
+            {props.title}
+            {{ childNodes } && <ul>{childNodes}</ul>}
+        </li>
     </>;
 
 }
