a8cc6a6210ea79603dd759fb358bb7d03e3c4235
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-01 15:16:22 -0300

    Replaced codecov-actions with custom upload coverage using bash uploader outside docker.

--- init.test.sh
+++ init.test.sh
@@ -4,5 +4,3 @@ coverage run --source='.' manage.py test
 #coverage report
 #coverage xml -o ./coverage_report/coverage.xml
 coverage xml
-curl -s https://codecov.io/bash > codecov.sh
-bash codecov.sh -t $CODECOV_TOKEN
