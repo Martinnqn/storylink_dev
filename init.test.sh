c6a5fae019d39c0fba894f11fb6195fc92612999
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-01 13:12:51 -0300

    BugFixing in progress. Testing sudo commands.

--- init.test.sh
+++ init.test.sh
@@ -2,4 +2,7 @@
 #python manage.py test
 coverage run --source='.' manage.py test
 #coverage report
-coverage xml -o ./coverage_report/coverage.xml
+cd coverage_report
+ls
+cd ..
+sudo coverage xml -o ./coverage_report/coverage.xml
