aa06279b4ea4a9195e7fda1cd834521283d0036c
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-01 15:53:01 -0300

    updated init.test.sh

--- init.test.sh
+++ init.test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #python manage.py test
 coverage run manage.py test
+echo 'terminaa'
 #coverage report
-coverage xml -o ./coverage_report/coverage.xml
+coverage erase
+echo 'borra'
+coverage xml -o /home/app/web/coverage_report/coverage.xml
