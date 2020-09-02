513e4b70555350d050dd5e2609cdd2cf32e2e744
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-02 09:58:52 -0300

    Non-root user in dockerfile.test, --no-deps removed and /github/home mounted.

--- init.test.sh
+++ init.test.sh
@@ -3,9 +3,9 @@
 coverage run manage.py test apps/users/
 echo 'crea'
 coverage report
-chmod -R +rwx /home/app/web/coverage_report/
 #coverage xml -o /home/app/web/coverage_report/coverage.xml
 coverage xml
 ls
 pwd
 mv coverage.xml /home/app/web/coverage_report/
+ls
