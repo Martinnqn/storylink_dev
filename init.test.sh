89a2e653d13152c4e7743abe3c17a45bd901a702
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-02 11:17:37 -0300

    init.test.sh modified for CI and local environment.

--- init.test.sh
+++ init.test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#python manage.py test
-coverage run manage.py test apps/users/
-echo 'crea'
-coverage report
 #coverage xml -o /home/app/web/coverage_report/coverage.xml
-coverage xml
-ls
-pwd
-mv coverage.xml /home/app/web/coverage_report/
-ls
+if [ ! -z $CI]
+then
+coverage run manage.py test apps/users/
+coverage report
+    coverage xml
+    mv coverage.xml /home/app/web/coverage_report/
+else
+    python manage.py test
+fi
