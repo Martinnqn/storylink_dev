b7978b750d2ae3831dda8eb20ff11a8d409c3846
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-02 16:38:41 -0300

    bugfixed init.test.sh

--- init.test.sh
+++ init.test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 #coverage xml -o /home/app/web/coverage_report/coverage.xml
-echo $CI
-if [ ! -z $CI]
+echo "$CI"
+if [ "$CI" ];
 then
-coverage run manage.py test apps/users/
-coverage report
+    echo "1"
+    coverage run manage.py test apps/users/
+    coverage report
     coverage xml
     mv coverage.xml /home/app/web/coverage_report/
 else
+    echo "2"
     python manage.py test
 fi
