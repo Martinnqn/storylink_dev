e39fde1b0eaccb484ae7b65876c523b55494ac75
Martinnqn <bermudez_martin.nqn@hotmail.com>
2020-09-04 13:07:03 -0300

    Begining frontend with create-react-app

--- st-front/src/js/index.js
+++ st-front/src/js/index.js
@@ -1,49 +1,19 @@
-import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import StoryTree from '../components/StoryTree.jsx'
-
-const StoryTreeMain = () => {
-  const [username, setUsername] = useState('')
-  const [pub, setPub] = useState('')
-  const [title, setTitle] = useState('')
-  const [isShowed, setIsShowed] = useState(false)
-
-  const show = () => {
-    setIsShowed(true);
-    const data = document.getElementById('view-tree-story').dataset;
-    setPub(data.pubid);
-  }
-
-  const hideIndexTree = () => {
-    setIsShowed(false);
-  }
-
-  useEffect(() => {
-    const title = document.getElementById('title-pub-detail').textContent;
-    setTitle(title);
-    const data = document.getElementById('view-tree-story').dataset;
-    setPub(data.pubid);
-    setUsername(data.username);
-  }, [pub]);
+import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import Root from '../components/Root.jsx'
 
 
-  const tree = (pub != '') ? <ul><StoryTree username={username} pubId={pub} type='story' title={title} /></ul> : null;
-
-  const classD = (isShowed) ? '' : 'hided-menu-pub';
-
-  return <div><a id="view-tree-story" data-pubid='' data-username='' className="btn-menu-pub"
-    href="javascript: void(0)" onClick={show}>
-    <span className="material-icons">account_tree</span>
-  </a>
-    <div className={"container " + classD} id="index-tree">
-      <a id="close-index-tree" className="" href="javascript: void(0)" onClick={hideIndexTree}>
-        <span className="material-icons">highlight_off</span>
-      </a>
-      {tree}
-    </div>
-  </div>
+const root = document.getElementById('root');
+if (root != undefined) {
+  ReactDOM.render(
+    <Root />,
+    root
+  );
 }
 
+/*
+import StoryTreeMain from '../components/storyTree/StoryTreeMain.jsx'
 
 const treeView = document.getElementById('tree-react');
 if (treeView != undefined) {
@@ -52,3 +22,4 @@ if (treeView != undefined) {
     treeView
   );
 }
+*/
